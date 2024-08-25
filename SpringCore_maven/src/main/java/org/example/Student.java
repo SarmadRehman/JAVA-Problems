c996a0de6cb7bf5a3ec5d44bd54f9bba32afa349
Sarmad Rehman <sarmadr02@gmail.com>
2024-08-25 22:13:19 +0500

    setter injection

---
+++ SpringCore_maven/src/main/java/org/example/Student.java
@@ -0,0 +1,52 @@
+package org.example;
+
+
+// this class is a bean to inject data
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String studentAddress;
+    // auto generated getters
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+    //auto generated setters
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    public Student(int studentId, String studentName, String studentAddress) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+    }
+
+    public Student() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                ", studentAddress='" + studentAddress + '\'' +
+                '}';
+    }
+}
